7b9a275caf3cd2c32434d76122e12e807b581288
Shree Mithra <shreemithra08@gmail.com>
2024-08-14 06:12:59 +0530

    initial commit

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -3,9 +3,14 @@ const express=require('express');
 const router=express.Router();
 const auth=require('../middlewares/auth');
 
+
 router.post("/",auth,CartController.createCart)
 router.get("/",auth,CartController.getcart)
 router.delete("/delete/:id",auth,CartController.deleteCart)
+
+// router.post("/",CartController.createCart)
+// router.get("/",CartController.getcart)
+// router.delete("/delete/:id",CartController.deleteCart)
 
 
 module.exports=router
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,9 +2,9 @@ const ProductController =require('../controllers/productController')
 const express=require('express');
 const router=express.Router();
 const auth=require('../middlewares/auth');
-
+//auth
 router.get("/",ProductController.getProducts)
-router.post("/",auth,ProductController.createProduct)
+router.post("/",ProductController.createProduct)
 // router.get('/products',()=>{
 
 // })
